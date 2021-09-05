f85c50d1596cce2007810656bcf156b4e5a39a15
Éder Lemke <eder.lemke@gmail.com>
2021-09-05 15:40:00 -0300

    Implementados métodos da tag Cob (conforme api bacen)

---
+++ src/ClientHttp.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Elemke\SicoobPix;
+
+class ClientHttp
+{
+    private $token;
+    private $certificadoPublico;
+    private $certificadoPrivado;
+    private $debug;
+
+    public function __construct(string $token, bool $debug = false)
+    {
+        $this->token = $token;
+        $this->debug = $debug;
+        $this->certificadoPublico = [$_ENV['SICOOBPIX_CAMINHO_CERT_PUBLICO'], $_ENV['SICOOBPIX_SENHA_CERT_PUBLICO']];
+        $this->certificadoPrivado = [$_ENV['SICOOBPIX_CAMINHO_CERT_PRIVADO'], $_ENV['SICOOBPIX_SENHA_CERT_PRIVADO']];
+    }
+
+    public function requisicao(string $metodo, string $url, array $body = null)
+    {
+        try {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request($metodo, $url,
+                [
+                    'json' => $body,
+                    'debug' => $this->debug ?? false,
+                    'headers' => [
+                        'Authorization' => "Bearer {$this->token}"
+                    ],
+                    'cert' => $this->certificadoPublico,
+                    'ssl_key' => $this->certificadoPrivado
+                ]
+            );
+            return $response->getBody()->getContents();
+        } catch (\Exception $exc) {
+            throw $exc;
+        }
+    }
+}
---
+++ src/Cob.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Elemke\SicoobPix;
+
+/**
+ * Class Cob reúne endpoints destinados a lidar com gerenciamento de cobranças imediatas.
+ * @package elemke\sicoobpix
+ */
+class Cob
+{
+
+    private $psp;
+    private $clientHttp;
+    public $debug = false;
+
+    public function __construct(Psp $psp)
+    {
+        $this->psp = $psp;
+        $this->clientHttp = new ClientHttp($psp->getToken(), $this->debug);
+    }
+
+    /**
+     * Criar cobrança imediata
+     * @param string $txId
+     * @param array $body
+     * @return string
+     * @throws \Exception
+     */
+    public function criar(array $body, string $txId = null): string
+    {
+        try {
+            $metodo = is_null($txId) ? 'POST' : 'PUT';
+            $url = is_null($txId) ? 'cob' : "cob/{$txId}";
+            return $this->clientHttp->requisicao($metodo, "{$this->psp->getUrlPix()}/{$url}", $body);
+        } catch (\Exception $exc) {
+            throw $exc;
+        }
+    }
+
+    /**
+     * Alterar cobrança imediata
+     * @param array $body
+     * @param string $txId
+     * @return string
+     * @throws \Exception
+     */
+    public function alterar(array $body, string $txId): string
+    {
+        try {
+            return $this->clientHttp->requisicao('PATCH', "{$this->psp->getUrlPix()}/cob/{$txId}", $body);
+        } catch (\Exception $exc) {
+            throw $exc;
+        }
+    }
+
+    /**
+     * Consultar cobranças imediatas
+     * @param string|null $txId
+     * @param array|null $parametros
+     * @return string
+     * @throws \Exception
+     */
+    public function consultar(string $txId = null, array $parametros = null): string
+    {
+        try {
+            if (is_null($txId) && is_null($parametros)) {
+                throw new \Exception('Obrigatório algum parâmetro para consulta');
+            }
+            if (!is_null($txId)) {
+                $url = "cob/{$txId}";
+            } else {
+                $queryString = http_build_query($parametros);
+                $url = "cob?{$queryString}";
+            }
+            return $this->clientHttp->requisicao('GET', "{$this->psp->getUrlPix()}/{$url}");
+        } catch (\Exception $exc) {
+            throw $exc;
+        }
+    }
+
+}
